5e7fd537637512667f71ed932e839db68b6e2478
zahra sahamivand <z.sahamivand81@gmail.com>
2024-07-08 01:59:30 +0330

    python code

---
+++ python.py
@@ -0,0 +1,12 @@
+text_dot = input()
+text = text_dot.replace("."," ")
+text_set = set(text.split())
+longest_word = 0 
+
+for j in text_set:
+    if len(j) > longest_word: 
+        longest_word = len(j)
+
+print("words: " + f"{len(text_set)}")
+print("longest: " + f"{longest_word}")
+print("chars: " + f"{len(text_dot)}")
